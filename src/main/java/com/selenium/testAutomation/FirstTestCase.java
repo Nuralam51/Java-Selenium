20ec1644f339fca9e20ffd770795c405ea95c2b7
Nuralam51 <mohammadnur4112@gmail.com>
2021-12-08 21:24:14 +0600

    make change

--- src/main/java/com/selenium/testAutomation/FirstTestCase.java
+++ src/main/java/com/selenium/testAutomation/FirstTestCase.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class SecondTestCase {
+public class FirstTestCase {
 
     public static void main(String[] args) throws InterruptedException {
 
